8622038ca4b531bca87d17c668669da6417af6ba
Mike Hamrah <mhamrah@bluecore.com>
2020-03-30 20:52:37 -0400

    add sigint handler

--- go.mod
+++ go.mod
@@ -7,5 +7,6 @@ require (
 	github.com/oklog/ulid v1.3.1
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.6.2
+	google.golang.org/genproto v0.0.0-20190819201941-24fa4b261c55
 	google.golang.org/grpc v1.28.0
 )
--- todos/server/cmd/main.go
+++ todos/server/cmd/main.go
@@ -4,15 +4,17 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"os"
+	"os/signal"
 
 	pb "github.com/mhamrah/grpc-example/gen"
-	"github.com/mhamrah/grpc-example/todos/server"
+	todos "github.com/mhamrah/grpc-example/todos/server"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
 
 func main() {
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 50051))
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 50052))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
@@ -21,5 +23,26 @@ func main() {
 
 	reflection.Register(grpcServer)
 
-	grpcServer.Serve(lis)
+	signalRunner(func() {
+		log.Println("Starting server...")
+		grpcServer.Serve(lis)
+	},
+		func() {
+			log.Println("bubye!")
+		})
+}
+
+func signalRunner(runner, stopper func()) {
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt, os.Kill)
+
+	go func() {
+		runner()
+	}()
+
+	log.Println("hit Ctrl-C to shutdown")
+	select {
+	case <-signals:
+		stopper()
+	}
 }
